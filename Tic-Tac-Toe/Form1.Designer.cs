30e81a1ffef043e25535c8dcfc3ea144baa22ad6
Tom Van Hoeydonck <sephiku1990@hotmail.com>
2013-10-02 21:40:07 +0200

    INITIAL COMMIT

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Inputbox
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.speler1 = new System.Windows.Forms.Label();
+            this.speler2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblscore1 = new System.Windows.Forms.Label();
+            this.lblscore2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(78, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Location = new System.Drawing.Point(144, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Location = new System.Drawing.Point(12, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 56);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Location = new System.Drawing.Point(78, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 56);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button6.Location = new System.Drawing.Point(144, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(60, 56);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button7.Location = new System.Drawing.Point(12, 180);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(60, 56);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button8.Location = new System.Drawing.Point(78, 180);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(60, 56);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.My_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button9.Location = new System.Drawing.Point(144, 180);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(60, 56);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.My_Click);
+            // 
+            // speler1
+            // 
+            this.speler1.AutoSize = true;
+            this.speler1.Location = new System.Drawing.Point(233, 56);
+            this.speler1.Name = "speler1";
+            this.speler1.Size = new System.Drawing.Size(45, 13);
+            this.speler1.TabIndex = 9;
+            this.speler1.Text = "Player 1";
+            // 
+            // speler2
+            // 
+            this.speler2.AutoSize = true;
+            this.speler2.Location = new System.Drawing.Point(328, 56);
+            this.speler2.Name = "speler2";
+            this.speler2.Size = new System.Drawing.Size(45, 13);
+            this.speler2.TabIndex = 10;
+            this.speler2.Text = "Player 2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(395, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // lblscore1
+            // 
+            this.lblscore1.AutoSize = true;
+            this.lblscore1.Location = new System.Drawing.Point(233, 99);
+            this.lblscore1.Name = "lblscore1";
+            this.lblscore1.Size = new System.Drawing.Size(35, 13);
+            this.lblscore1.TabIndex = 12;
+            this.lblscore1.Text = "label1";
+            // 
+            // lblscore2
+            // 
+            this.lblscore2.AutoSize = true;
+            this.lblscore2.Location = new System.Drawing.Point(328, 99);
+            this.lblscore2.Name = "lblscore2";
+            this.lblscore2.Size = new System.Drawing.Size(35, 13);
+            this.lblscore2.TabIndex = 13;
+            this.lblscore2.Text = "label2";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 350);
+            this.Controls.Add(this.lblscore2);
+            this.Controls.Add(this.lblscore1);
+            this.Controls.Add(this.speler2);
+            this.Controls.Add(this.speler1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TicTacToe";
+            this.Text = "Tic-Tac-Toe";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label speler1;
+        private System.Windows.Forms.Label speler2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Label lblscore1;
+        private System.Windows.Forms.Label lblscore2;
+    }
+}
+
---
+++ Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Inputbox
+{
+    public partial class TicTacToe : Form
+    {
+        private int teller;
+        private int score1;
+        private int score2;
+        public TicTacToe()
+
+        {
+            InitializeComponent();
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            lblscore1.Text = "";
+            lblscore2.Text = "";
+            score1 = Convert.ToInt32(lblscore1.Text);
+            score2 = Convert.ToInt32(lblscore2.Text);
+        }
+        private void reset()
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            speler1.ForeColor = Color.Red;
+            speler2.ForeColor = Color.Black;
+            teller = 1;
+        }
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            reset();
+            lblscore2.Text = "0";
+            lblscore1.Text = "0";
+        }
+
+        public void My_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.Enabled = false;
+            if (teller % 2 == 0)
+            {
+                teller++;
+                button.Text = "X";
+                check();
+                speler1.ForeColor = Color.Red;
+                speler2.ForeColor = Color.Black;
+            }
+            else
+            {
+                teller++;
+                button.Text = "O";
+                check();
+                speler2.ForeColor = Color.Red;
+                speler1.ForeColor = Color.Black;
+            }
+        }
+        private void check()
+        {
+            if (button1.Text == "X" && button2.Text == "X" && button3.Text == "X" || button1.Text == "X" && button5.Text == "X" && button9.Text == "X" || button1.Text == "X" && button4.Text == "X" && button7.Text == "X"
+                || button2.Text == "X" && button5.Text == "X" && button8.Text == "X" || button3.Text == "X" && button6.Text == "X" && button9.Text == "X" || button1.Text == "X" && button5.Text == "X" && button7.Text == "X"
+                || button4.Text == "X" && button5.Text == "X" && button6.Text == "X" || button3.Text == "X" && button5.Text == "X" && button7.Text == "X")
+            {
+                    MessageBox.Show("Speler 2 heeft gewonnen!");
+                    score2 += 1;
+                    lblscore2.Text = Convert.ToString(score2);
+               
+                 DialogResult result = MessageBox.Show("Wil je nog een spel spelen?", "Spel gedaan!", MessageBoxButtons.YesNo);
+                 if (result == DialogResult.Yes)
+                 {
+                     reset();
+                 }
+                 else
+                 {
+                     if (score1 > score2)
+                     {
+                         MessageBox.Show("Speler 1 is de winnaar!");
+                     }
+                     if (score1 == score2)
+                     {
+                         MessageBox.Show("Gelijkspel!");
+                     }
+                     if (score1 < score2)
+                     {
+                         MessageBox.Show("Speler 2 is de winnaar!");
+                     }
+                     this.Close();
+            }
+                
+        }
+            if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O" || button1.Text == "O" && button5.Text == "O" && button9.Text == "O" || button1.Text == "O" && button4.Text == "O" && button7.Text == "O"
+                || button2.Text == "O" && button5.Text == "O" && button8.Text == "O" || button3.Text == "O" && button6.Text == "O" && button9.Text == "O" || button1.Text == "O" && button5.Text == "O" && button7.Text == "O"
+                || button4.Text == "O" && button5.Text == "O" && button6.Text == "O" || button3.Text == "O" && button5.Text == "O" && button7.Text == "O")
+            {
+                MessageBox.Show("Speler 1 heeft gewonnen!");
+                score1 += 1;
+                lblscore1.Text = Convert.ToString(score1);
+
+                DialogResult result = MessageBox.Show("Wil je nog een spel spelen?", "Spel gedaan!", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    reset();
+                }
+                else
+                {
+                    if (score1 >= score2)
+                    {
+                        MessageBox.Show("Speler 1 is de winnaar!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Speler 2 is de winnaar!");
+                    }
+                    this.Close();
+                }
+                
+            }
+    }
+    }
+}
